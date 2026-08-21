6989a4b1517115f4d975b164f78ed4157b5ccf3e
KA <kararammmar58@gmail.com>
2026-08-22 00:19:19 +0300

    Final fix for component imports

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ToolsGrid from "./components/ToolsGrid";
 
 export default function Home() {
   return (
-    <main style={{ position: "relative" }}>
+    <main style={{ position: "relative", backgroundColor: "#020617", minHeight: "100vh" }}>
       <Hero />
       <ToolsGrid />
     </main>
